e6d55b8c999cf09063e5bdfe0c3f3065f11f1c48
Levinton Slava <slevinto@gmail.com>
2021-10-07 16:25:24 +0300

    some desiiiign improve

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,23 +13,23 @@ export default function Projects() {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4">
               הפרויקטים שלי 
           </h1>          
-        </div>
-        <Fade right cascade>
+        </div>        
           <div className="text-center max-w-sm rounded shadow-lg mx-auto">
             {projects.map((project) => (
-              <a href={project.link} key={project.image} className="p-4">
-                  <img className="mx-auto pb-4" src={project.image}/>                
-                  <h1 className="title-font text-lg font-medium text-black mb-3">
+              <Fade right cascade>
+                <a href={project.link} key={project.image} className="p-4">
+                  <img className="mx-auto w-16 h-16" src={project.image}/>                
+                  <h1 className="title-font text-lg font-medium text-black mb-3 pt-4">
                       {project.title}
                   </h1>
                   <h2 className="tracking-widest text-md title-font font-semibold text-gray-800 mb-1 pb-1">
                       {project.subtitle}
                   </h2>
                   <p className="leading-relaxed pb-4">{project.description}</p>
-              </a>
+                </a>
+              </Fade>
             ))}
-          </div> 
-        </Fade> 
+          </div>          
         </div>      
      </section>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,10 +15,10 @@ export default function Skills() {
             ידע &amp; כלים
           </h1>          
         </div>
-        <Fade right cascade>
           <div className="text-center max-w-sm rounded shadow-lg mx-auto">
             {skills.map((skill) => (
-              <div key={skill.title} className="bg-gray-800 my-8 pt-4">
+              <Fade right cascade>
+                <div key={skill.title} className="bg-gray-800 my-8 pt-4">
                   <div className="max-w-sm rounded overflow-hidden shadow-lg">
                     <FontAwesomeIcon icon={skill.skillIcon} className="text-green-400 text-3xl"/>
                     <div className="px-6 py-4">
@@ -30,10 +30,10 @@ export default function Skills() {
                       </p>
                     </div>                  
                   </div>
-              </div>
+                </div>
+              </Fade>
             ))}
-          </div> 
-        </Fade> 
+          </div>          
         </div>      
     </section>
   );
